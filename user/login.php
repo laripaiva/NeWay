3007c1373d894e9127ce58e61d04971969339be2
Gustavo Garcia <gustavo.ggdrn@gmail.com>
2018-06-12 23:57:09 -0300

    [UPDATE] Front-end implementado em login e registro

--- user/login.php
+++ user/login.php
@@ -6,63 +6,97 @@ require('../_app/Config.inc.php');
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <meta charset="UTF-8">
-        <title>Login</title>
-        <link rel="stylesheet" href="css/reset.css" />
-        <link rel="stylesheet" href="css/admin.css" />
-        <link rel="stylesheet" type="text/css" href="css\style.css" />
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title></title>
+      <link rel="shortcut icon" href="" type="image/x-icon"/>
+    	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    	<link rel="stylesheet" href="css/login.css">
+      <link rel="stylesheet" href="css/reset.css" />
+      <link rel="stylesheet" href="css/admin.css" />
+      <link rel="stylesheet" type="text/css" href="css/style.css" />
 
     </head>
-    <body class="login">
-        <div id="login">
-            <h1>Login</h1>
-            <form name="UserLoginForm" action="" method="post">
-            <label>
-                <span>E-mail:</span>
-                <input type="email" name="email" id="email"/>
-            </label>
-            <label>
-                <span>Senha:</span>
-                <input type="password" name="pass" id="pass"/>
-            </label>
+    <body>
+    <header>
+  		<nav>
+  			<div class="nav-wrapper">
+  				<a href="#!" class="brand-logo">NeWay</a>
+  				<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+  				<ul class="right hide-on-med-and-down">
+  					<li><a href="index.php">Home</a></li>
+  					<li><a href="register.php">Cadastre-se</a></li>
+  				</ul>
+  			</div>
+  		</nav>
+  		<ul class="sidenav" id="mobile-demo">
+  			<li><a href="badges.html">Home</a></li>
+  			<li><a href="sass.html">Cadastre-se</a></li>
+  		</ul>
+  	</header>
 
-            <?php
-                $login = new Login(2);
+	   <main class="main">
+        <form class="form z-depth-5" name="UserLoginForm" action="" method="post">
+          <div class="container">
 
-                if($login->checkLogin()){
-                    header('Location: dashboard.php');
+  					  <div class="input-field ">
+              <i class="material-icons prefix">assignment_ind</i>
+              <input type="email" name="email" id="icon_prefix" type="text" class="validate">
+              <label for="icon_prefix">Login</label>
+              <!--<input type="email" name="email" id="email"/> -->
+            </div>
+
+            <div class="input-field ">
+              <i class="material-icons prefix">lock_outline</i>
+              <input id="icon_telephone" type="password" name="pass" id="pass" class="validate">
+              <label for="icon_telephone">Password</label>
+              <!--<input type="password" name="pass" id="pass"/> -->
+              <a id="logar" type="submit" name="UserLogin" value="Logar" class="waves-effect waves-light btn center-align"><i class="material-icons right">cloud</i>Logar</a>
+    				</div>
+
+        <?php
+            $login = new Login(2);
+
+            if($login->checkLogin()){
+                header('Location: dashboard.php');
+            }
+
+            $dataLogin = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+            if (!empty($dataLogin['UserLogin'])){
+                $login->exeLogin($dataLogin);
+                if (!$login->getResult()){
+                    frontErro($login->getError()[0], $login->getError()[1]);
+                    frontErro("Seu pagamento ainda não foi identificado. Envie o comprovante para o email @exemplo.", E_USER_WARNING);
+                    $userId = $login->getId();
+                    $readUser = new Read;
+                    $readUser->exeRead("users", "WHERE id = :id", "id={$userId}");
+                    $user = $readUser->getResult()[0];
+        ?>
+            Clique <a href="segunda-via.php?aluno=<?php echo $user['id'];?>&nome=<?php echo $user['nome'];?>&sobrenome=<?php echo $user['nome_final'];?>">aqui</a> para imprimir a segunda via do boleto.
+        <?php
+                }else{
+                    header('Location: dashboard.php?exe=index');
                 }
+            }
 
-                $dataLogin = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-                if (!empty($dataLogin['UserLogin'])){
-                    $login->exeLogin($dataLogin);   
-                    if (!$login->getResult()){
-                        frontErro($login->getError()[0], $login->getError()[1]);
-                        frontErro("Seu pagamento ainda não foi identificado. Envie o comprovante para o email @exemplo.", E_USER_WARNING);
-                        $userId = $login->getId();
-                        $readUser = new Read;
-                        $readUser->exeRead("users", "WHERE id = :id", "id={$userId}");
-                        $user = $readUser->getResult()[0];
-            ?>
-                Clique <a href="segunda-via.php?aluno=<?php echo $user['id'];?>&nome=<?php echo $user['nome'];?>&sobrenome=<?php echo $user['nome_final'];?>">aqui</a> para imprimir a segunda via do boleto.
-            <?php 
-                    }else{
-                        header('Location: dashboard.php?exe=index');
-                    }
+            $get = filter_input (INPUT_GET, 'exe', FILTER_DEFAULT);
+            if (!empty($get)){
+                if ($get=='restrito'){
+                    frontErro("<b>Acesso negado!</b> Realize login para entrar no sistema!", E_USER_NOTICE);
+                }elseif($get=='logoff'){
+                    frontErro("<b>Deslogado com sucesso!</b> Sua sessão foi finalizada.", ACCEPT);
                 }
-
-                $get = filter_input (INPUT_GET, 'exe', FILTER_DEFAULT);
-                if (!empty($get)){
-                    if ($get=='restrito'){
-                        frontErro("<b>Acesso negado!</b> Realize login para entrar no sistema!", E_USER_NOTICE); 
-                    }elseif($get=='logoff'){
-                        frontErro("<b>Deslogado com sucesso!</b> Sua sessão foi finalizada.", ACCEPT);
-                    }
-                }
-            ?>
-            <input type="submit" name="UserLogin" value="Logar"/>
-                    
-            </form>
-        </div>
-    </body>
+            }
+        ?>
+        <!--<input type="submit" name="UserLogin" value="Logar"/>-->
+      </div>
+      </form>
+  </main>
+  <script src="./js/jquery-3.3.1.js" type="text/javascript" charset="utf-8" async defer></script>
+  <script src="./js/materialize.js"></script>
+  <script src="./js/login.js"></script>
+</body>
 </html>
--- user/register.php
+++ user/register.php
@@ -5,47 +5,82 @@ require('../_app/Config.inc.php');
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <meta charset="UTF-8">
-        <title>Login</title>
-        <link rel="stylesheet" href="css/reset.css" />
-        <link rel="stylesheet" href="css/admin.css" />
-        <link rel="stylesheet" type="text/css" href="css\style.css" />
-        <script type="text/javascript" src="./js/cep.js"></script>
+    	<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    	<title></title>
+    	<link rel="shortcut icon" href="" type="image/x-icon"/>
+    	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    	<link rel="stylesheet" href="css/cad.css">
     </head>
     <body class="register">
-        <div id="register">
-            <h1>Cadastro</h1>
-            <form name="UserRegisterForm" action="" method="post">
-            <label>
-                <span>Nome:</span>
-                <input type="name" name="name" id="name"/>
-            </label>
-            <br><br>
-            <label>
-                <span>Nome Final:</span>
-                <input type="last_name" name="final_name" id="final_name"/>
-            </label>
-            <br><br>
-            <label>
-                <span>E-mail:</span>
-                <input type="email" name="email" id="email"/>
-            </label>
-            <br><br>
-            <label>
-                <span>Senha:</span>
-                <input type="password" name="pass" id="pass"/>
-            </label>
-            <br><br>
-            <label>
-                <span>Confirmar Senha:</span>
-                <input type="password" name="pass2" id="pass2"/>
-            </label>
-            <input type="submit" name="UserRegister" value="Cadastrar"/>
+      <header>
+    		<nav>
+    			<div class="nav-wrapper">
+    				<a href="#!" class="brand-logo">NeWay</a>
+    				<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    				<ul class="right hide-on-med-and-down">
+    					<li><a href="index.php">Home</a></li>
+    					<li><a href="register.php">Cadastre-se</a></li>
+    				</ul>
+    			</div>
+    		</nav>
+    		<ul class="sidenav" id="mobile-demo">
+    			<li><a href="badges.html">Home</a></li>
+    			<li><a href="sass.html">Cadastre-se</a></li>
+    		</ul>
+    	</header>
+      <main class="main">
+          <form class="form z-depth-5" name="UserRegisterForm" action="" method="post">
+            <div class="container">
+
+              <div class="input-field ">
+                <i class="material-icons prefix">assignment_ind</i>
+    						<input name="name" id="icon_prefix" type="text" class="validate">
+    						<label for="icon_prefix">Nome</label>
+    					</div>
+                <!--<input type="name" name="name" id="name"/>-->
+
+                <div class="input-field ">
+      						<i class="material-icons prefix">assignment_ind</i>
+      						<input type="text" name="final_name" id="icon_prefix"  class="validate">
+      						<label for="icon_prefix">Sobrenome</label>
+      					</div>
+
+                <!-- <input type="last_name" name="final_name" id="final_name"/> -->
+
+                <div class="input-field ">
+      						<i class="material-icons prefix">mail</i>
+      						<input type="email" name="email" id="icon_prefix" type="text" class="validate">
+      						<label for="icon_prefix">E-mail</label>
+      					</div>
+
+                <!--<input type="email" name="email" id="email"/> -->
+                <div class="input-field ">
+      						<i class="material-icons prefix">lock_outline</i>
+      						<input  type="password" name="pass" id="icon_telephone" type="tel" class="validate">
+      						<label for="icon_telephone">Senha</label>
+      					</div>
+
+                <!--<input type="password" name="pass" id="pass"/> -->
+                <div class="input-field ">
+                  <i class="material-icons prefix">lock_outline</i>
+                  <input type="password" name="pass2" id="icon_telephone" type="tel" class="validate">
+                  <label for="icon_telephone">Confirmar Senha</label>
+                </div>
+                <!--
+                <button class="waves-effect waves-light btn center-align" type="submit" name="UserRegister">Cadastrar
+                  <i class="material-icons right">send</i>
+                </button> -->
+            <!-- <a id="logar" type="submit" name="UserRegister" class="waves-effect waves-light btn center-align"><i class="material-icons right">person_add</i>Cadastrar</a> -->
+            <input type="submit" name="UserRegister" value="Cadastrar" class="waves-effect waves-light btn center-align"/>
             </form>
             <?php
                 $register = new Register;
                 $dataRegister = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-                var_dump ($dataRegister);
+                //var_dump ($dataRegister);
 
                 if (!empty($dataRegister['UserRegister'])){
                   if (strlen($dataRegister['name']) < 2){
@@ -119,5 +154,14 @@ require('../_app/Config.inc.php');
             }
             ?>
         </div>
+        <footer>
+
+
+        </footer>
+
+        <script src="JS/jquery-3.3.1.js" type="text/javascript" charset="utf-8" async defer></script>
+        <script src="JS/materialize.js"></script>
+        <script src="JS/cad.js"></script>
+        <script src="JS/cep.js"></script>
     </body>
 </html>
