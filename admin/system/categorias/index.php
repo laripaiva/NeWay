67a169396baa539c4f3b4a76235d78bea7cfe25e
Ana Clara <claracorreadasilva@gmail.com>
2018-06-15 00:07:30 -0300

    [style] upgrade style fot categories table

--- admin/system/categorias/index.php
+++ admin/system/categorias/index.php
@@ -1,8 +1,8 @@
 <div class="neway z-depth-5">
-		<p class="title center-align">Gerenciar categorias</p>
-	</div>
+  <p class="title center-align">Gerenciar categorias</p>
+</div>
 <div class="container">
-        
+
     <?php
         /************************************************************************************************
         * VERIFICAR SE ERROS E SUCESSOS NO CADASTRAMENTO  E ATUALIZAÇÃO DE CATEGORIA
@@ -24,41 +24,49 @@
         }elseif($empty){
             frontErro("<b> Erro </b>, você tentou atualizar uma categoria que não existe.", E_USER_WARNING);
         }
-    ?>
-    <div class="row">
-        <div class="col l12">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Identificador </th>
-                        <th>Categoria</th>
-                        <th>Ação</th>
-                    </tr>
-                </thead>
-                <tbody>
-    <?php  
-        $readData = new Read;
-        $readData->exeRead("categorys", "WHERE id != :d", "d=0");
-
-        foreach ($readData->getResult() as $ses){
-            extract($ses);
-        
-    ?>
-                <tr id="">
-                    <td> <?=$id;?></td>
-                    <td> <?=$nome;?></td>
-                    <td><a class="waves-effect waves-teal btn-flat" href="painel.php?exe=categorias/update&categoria=<?=$id?>">Editar</a></td>
+        ?>
+        <style type="text/css">
+        tr:hover {
+          background-color:#e8eaf6;
+      }
+  </style>
+  <div class="row">
+    <div class="col l12">
+        <table class="centered">
+            <thead>
+                <tr>
+                    <th>Identificador </th>
+                    <th>Categoria</th>
+                    <th>Ação</th>
                 </tr>
-    
-    <?php 
-        }
-    ?>
-                </tbody>
-            </table>
+            </thead>
+            <tbody>
+                <?php  
+                $readData = new Read;
+                $readData->exeRead("categorys", "WHERE id != :d", "d=0");
 
-        </div>
+                foreach ($readData->getResult() as $ses){
+                    extract($ses);
+
+                    ?>
+                    <tr id="">
+                        <td> <?=$id;?></td>
+                        <td> <?=$nome;?></td>
+                        <td><a class="waves-effect red darken-4 btn" href="painel.php?exe=categorias/update&categoria=<?=$id?>">Editar</a></td>
+                    </tr>
+
+                    <?php 
+                }
+                ?>
+            </tbody>
+        </table>
+
     </div>
-    <a href="painel.php?exe=categorias/create" class="waves-effect waves-light btn modal-trigger"><i class="material-icons right">cloud</i>Criar Categoria</a>
+</div>
+<div class="center-align">
+    <a href="painel.php?exe=categorias/create" class="waves-effect light-blue darken-4 btn modal-trigger"><i class="material-icons right">add_to_queue</i>Criar Categoria</a>    
+</div>
+
 </div>
 
 </div>
